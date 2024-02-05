842f428951140f34c76e5b7f1c6a60113e600155
duardoliveiras <duardooliveiras@gmail.com>
2024-02-05 13:54:16 +0000

    start spring

---
+++ backend/angstore/src/main/java/com/angstore/AngstoreApplication.java
@@ -0,0 +1,13 @@
+package com.angstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngstoreApplication.class, args);
+	}
+
+}
---
+++ backend/angstore/src/test/java/com/angstore/AngstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
