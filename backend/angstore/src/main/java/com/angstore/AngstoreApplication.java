51020e6dd10444b36da3a08a0e212953b59f4281
duardoliveiras <duardooliveiras@gmail.com>
2024-02-05 17:59:07 +0000

    repository findByEmail

--- backend/angstore/src/main/java/com/angstore/AngstoreApplication.java
+++ backend/angstore/src/main/java/com/angstore/AngstoreApplication.java
@@ -32,22 +32,34 @@ public class AngstoreApplication {
 				address, 
 				email
 				);
+			// usingQueriesMongo(mongoTemplate, client, repository);
 
-			Query query = new Query();
-			query.addCriteria(Criteria.where("email").is(email));
-
-			List<Client> clients = mongoTemplate.find(query, Client.class);
-
-			if(clients.isEmpty()){
-				repository.insert(client);
-			}else{
-				throw new IllegalStateException("Client already exists");
-			}
-
-
-			
+			// ifPresentOrElse is a method that returns a value if a value is present, and otherwise returns a default value.
+			// The first argument is a Consumer that is executed if a value is present, and the second argument is a Runnable that is executed if a value is not present.
+			repository.findByEmail(email).ifPresentOrElse(
+				c -> {
+					throw new IllegalStateException("Client already exists");
+				},
+				() -> {
+					repository.insert(client);
+				
+				});
 	
 		};
 		
 	}
+
+	public void usingQueriesMongo(MongoTemplate mongoTemplate, Client client, ClientRepository repository){
+		Query query = new Query();
+		query.addCriteria(Criteria.where("email").is(client.getEmail()));
+
+		List<Client> clients = mongoTemplate.find(query, Client.class);
+
+		if(clients.isEmpty()){
+			repository.insert(client);
+		}else{
+			throw new IllegalStateException("Client already exists");
+		}
+	}
+
 }
--- backend/angstore/src/main/java/com/angstore/ClientRepository.java
+++ backend/angstore/src/main/java/com/angstore/ClientRepository.java
@@ -1,9 +1,19 @@
 package com.angstore;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+// import org.springframework.data.mongodb.repository.Query;
+
+
 
 // This interface extends the MongoRepository interface, which is a part of Spring Data MongoDB.
 // First argument is the type of the entity that the repository manages, and the second argument is the type of the unique identifier of the entity.
 public interface ClientRepository extends MongoRepository<Client, String>{
-    
+ 
+    Optional<Client> findByEmail(String email);
+
+    // @Query is used to define a custom query using MongoDB query language.
+    // @Query("show dbs")
+    // void test();
 }
