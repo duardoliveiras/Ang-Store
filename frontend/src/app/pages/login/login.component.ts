6f8ad5a3601a66c0d47b700e4fd6ef3ed4ac3b1c
duardoliveiras <duardooliveiras@gmail.com>
2024-02-16 17:39:43 +0000

    login form complete

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  form: FormGroup;
+  
+  constructor(private fb: FormBuilder){
+    // Validators.required is for required fields and Validators.email is for email format
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)  ]]
+    });
+  }
 
+
+  async submit(){
+    console.log(this.form.value);
+  }
+
+  
 }
